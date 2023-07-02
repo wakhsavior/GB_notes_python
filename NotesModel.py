ca2f1e21768d2fddb2a7e40efe05674c2a27b590
Saveliev Denis <wakhsavior@gmail.com>
2023-07-02 18:22:32 +0300

    Add comments

--- NotesModel.py
+++ NotesModel.py
@@ -2,7 +2,7 @@ import Note
 import json
 from os import path
 
-
+# Class Model for Notes, create Notes based class Note
 class NotesModel(object):
     def __init__(self):
         self.curId = 0
@@ -11,12 +11,13 @@ class NotesModel(object):
         self.items = []
         self.readNotesFromFile()
 
+# Load Note from data. Set date from data.
     def loadNote(self, id, date, title, msg):
         if self.curId < id:
             self.curId = id
         item = Note.Note.loadNote(id, date, title, msg)
         self.items.append(item)
-
+# Add new Note.
     def addNote(self, id, title, msg):
         lenght = len(self.items)
         if id == 0 or id >= self.curId:
@@ -29,7 +30,7 @@ class NotesModel(object):
             return 0
         else:
             return 1
-
+# Delete Note. Can receive ID or object Note. Check input data before remove.
     def delNote(self, note):
         i = 0
         if (isinstance(note, Note.Note) and self.items.count(note) > 0):
@@ -46,30 +47,34 @@ class NotesModel(object):
                     i += 1
                     self.saved = False
         return i
-
+# Get all note and give it to Presenter
     def getAllNotes(self):
         return self.items
-
+# Get one note with ID.
     def getNote(self, idNote):
         for item in self.items:
             if item.getNoteId() == idNote:
                 return item
         return 0
-
+# Get note id with object Note
     def getNoteId(self, note):
         for item in self.items:
             if item == note:
                 return item.getNoteId()
         return 0
+# Get base status change
     def saveStatus(self):
         return self.saved
 
+# Get current base filename
     def getFileName(self):
         return self.file_base
 
+# Adjust new file name
     def setFileName(self, filename):
         self.file_base = filename
 
+# Save current notes to file, use JSON format
     def saveNotesToFile(self):
         jsonNotes = []
         for item in self.items:
@@ -82,13 +87,16 @@ class NotesModel(object):
             json.dump(jsonNotes, noteFile)
             self.saved = True
         return 0
+# Delete all notes from list and set CurId to 0
     def clearAllNotes(self):
         self.items.clear()
         self.curId = 0
         return self.curId
+# Return current ID
     def getCurId(self):
         return self.curId
 
+# Read Notes from file, Use JSON format
     def readNotesFromFile(self):
         if path.exists(self.getFileName()):
             self.clearAllNotes()
--- NotesPresenter.py
+++ NotesPresenter.py
@@ -4,7 +4,7 @@ class NotesPresenter():
     def __init__(self, model, view):
         self.model = model
         self.view = view
-
+# Add notes, check succesfullness.
     def addNote(self):
         id = 0
         title = self.view.get_string('Enter Title for note: ')
@@ -12,7 +12,7 @@ class NotesPresenter():
         result = self.model.addNote(id, title, msg)
         if result == 0:
             self.view.showMessage("Note successfully added.")
-
+# Delete notes, check succesfullness.
     def delNote(self, id):
         result = self.model.delNote(id)
         if result == 0:
@@ -20,6 +20,7 @@ class NotesPresenter():
         else:
             self.view.showMessage(format(f"Note === {id} === successfully deleted."))
 
+# Change notes, check succesfullness.
     def changeNote(self, idNote):
         result = self.model.delNote(idNote)
         if result != 0:
@@ -28,18 +29,18 @@ class NotesPresenter():
             self.model.addNote(idNote, title, msg)
         else:
             self.view.showMessage("Wrong note number.")
-
+# Show all notes using View
     def showAllNotes(self):
         notes = self.model.getAllNotes()
         self.view.showAllNotes(notes)
-
+# Clear all notes
     def clearAllNotes(self):
         if self.model.clearAllNotes() == 0:
             self.view.showMessage("All Notes succesfully deleted.")
-
+# Show note using View with ID
     def showNote(self, id):
         self.view.showNote(self.model.getNote(id))
-
+# Export data to another file
     def exportNotes(self):
         filename = self.view.get_string("Enter filename to export Notes: ")
         self.model.setFileName(filename)
@@ -51,7 +52,7 @@ class NotesPresenter():
         result = self.model.saveNotesToFile()
         if result == 0:
             self.view.showMessage(format(f'Notes succesfully saved to {self.model.getFileName()}.'))
-
+# Load notes from file use model load. Check load status.
     def loadNotes(self):
         filename = self.view.get_string("Enter filename to load Notes: ")
         self.model.setFileName(filename)
@@ -61,6 +62,7 @@ class NotesPresenter():
         elif result == 1:
             self.view.showMessage(format(f'File {filename} not found.'))
 
+# Exit method, check data save using flag
     def exit(self):
         if not self.model.saveStatus():
             self.view.showMessage("You have unsaved Notes. Would you like to save it?")
